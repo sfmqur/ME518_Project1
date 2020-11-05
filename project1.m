e267c0ba407fac30d9fa434d54278b65ad87a00a
sfmqur <sammaruska@gmail.com>
2020-11-05 14:52:23 -0600

    optomized runtime and getting a variety of solutions from the total pool

--- project1.m
+++ project1.m
@@ -8,20 +8,20 @@
 tic
 clear;
 alpha2 = 5; %degrees
-p1x = 9; %inches
-p1y = 17.5; %inches
+p1x = 10; %inches
+p1y = 15; %inches
 p2x = 4; %inches
-p2y = 16; %inches
+p2y = 15; %inches
 space = 18; %inches, sidelength of the containment square area
 
 %max number of solutions to be checked, default settings yields 1.5e9 possible solutions
-% my PC, gaming computer, quite fast checks 1.4e6 solutions per min
-num_solutions = 1000; %number of desired solutions possible solutions to be saved.  
-solutions_to_check = 10e6; 
-
+%ERROR: if num_solutions is larger than your PC memory capacity the program
+% will through an error
+num_solutions = 20; %number of desired solutions possible solutions to be saved., if -1 it will equal solutions to check
+solutions_to_check = 1e7; % if value is -1, it checks all possible solutions
 
 step = 1; %degree, step size of each iteration
-%part 1, max assumption of variables
+%part 1, max assumption of variables 180 default for all values
 theta_max = 180; %degrees
 phi_max = 180; %degrees
 beta2_max = 180; %degrees
@@ -48,7 +48,7 @@ end
 
 %part 1 
 data_pointer = 1;
-wz_data = zeros(theta_max*beta2_max * phi_max, 8); %preallocate max size to improve runtime
+wz_data = zeros(theta_max*beta2_max * phi_max, 11); %preallocate max size to improve runtime
 
 for theta = 0:step:theta_max %iterate of all values of theta, phi and beta2
    for phi = 0:step:phi_max
@@ -74,7 +74,7 @@ for theta = 0:step:theta_max %iterate of all values of theta, phi and beta2
           if a2x > 0 && a2x < space && a2y > 0 && a2y < space && w > 0 && z > 0
               if a1x > 0 && a1x < space && a1y > 0 && a1y < space
                  if otwoy > 0 && otwoy < space && otwox > 0 && otwox < space
-                     temp = [w,z,a1x,a1y,a2x,a2y,otwox,otwoy]; % wz solutions array output
+                     temp = [w,z,a1x,a1y,a2x,a2y,otwox,otwoy,theta, phi, beta2]; % wz solutions array output
                      wz_data(data_pointer,:) = temp; 
                      data_pointer = data_pointer +1;
                  end
@@ -92,7 +92,7 @@ wz_data = wz_data(1:(data_pointer-1), :);
 %same for part 1 and part 2 the solutions generated will be the same, could
 %speed the program up if that is assumed to be true. and just combine 
 data_pointer = 1;
-us_data = zeros(sigma_max*gamma2_max*psi_max, 8);
+us_data = zeros(sigma_max*gamma2_max*psi_max, 11);
 
 for sigma = 0:step:sigma_max
    for gamma2 = 0:step:gamma2_max
@@ -118,7 +118,7 @@ for sigma = 0:step:sigma_max
           if b2x > 0 && b2x < space && b2y > 0 && b2y < space && u > 0 && s > 0
               if b1x > 0 && b1x < space && b1y > 0 && b1y < space
                  if ofourx > 0 && ofourx < space && ofoury > 0 && ofoury < space
-                     temp = [u,s,b1x,b1y,b2x,b2y,ofourx,ofoury]; %data file colums output
+                     temp = [u,s,b1x,b1y,b2x,b2y,ofourx,ofoury,sigma,gamma2,psi]; %data file colums output
                      us_data(data_pointer,:) = temp;
                      data_pointer = data_pointer +1;
                  end
@@ -132,20 +132,33 @@ end
 %us_data size reduction
 us_data = us_data(1:(data_pointer-1), :);
 
+possible_solutions = length(us_data(:,11)) * length(wz_data(:,11));
+if solutions_to_check == -1 || solutions_to_check > possible_solutions
+   solutions_to_check = possible_solutions; 
+end
+if num_solutions == -1 || num_solutions > solutions_to_check
+    num_solutions = solutions_to_check;
+end
+
+solutions = zeros(num_solutions,25 ); %preallocate memory for solution matrix and it vastly improves runtime, i cut unused size down later
+min_solution = 15000* ones(1,25); %preallocate min solution
+ 
+%in order to get varied solutions, a probablility factor is created and
+%check against a random probablility
+prob = num_solutions/solutions_to_check *1; %the integer is just a weight factor: ensures that the array is filled 
+
+%in order to get varied solutions, set step size to step across possible
+%wz/us solutions, so the whole solution set of 1e9 can be checked
+dus = sqrt(possible_solutions/solutions_to_check);
+dus = floor(dus);
+dwz = dus;
+
 %combonation of the two solution sets, need all criteria to be met
 data_pointer = 1;
-%set to max size allowd as 1.5B is too large and MATLAB complains
-% seem to be about 6.897127e3 rows / MB I set the max size of the array to
-% be about 1GB
-rows_mb = 6.897126969e3;
-
-solutions = zeros(num_solutions,19 ); %preallocate memory for solution matrix and it vastly improves runtime, i cut unused size down later
-min_solution = 15000* ones(1,19); %preallocate min solution
-possible_solutions = length(us_data(:,8)) * length(wz_data(:,8)); 
-
-for us = 1:length(us_data(:,8)) %tells up number of us solutions
-    for wz = 1:length(wz_data(:,8)) %tells up number of wz solutions
-        solutions_checked = us*length(wz_data(:,8)) + wz; 
+solutions_checked = 0;
+for us = 1:dus:length(us_data(:,8)) %tells up number of us solutions
+    for wz = 1:dwz:length(wz_data(:,8)) %tells up number of wz solutions
+        solutions_checked = solutions_checked +1; 
         %%%!!!! require that b1 must be +x (to the right) of a1 and that v1
         % and g1 are non zero
         % solution index comment on end of line
@@ -168,15 +181,17 @@ for us = 1:length(us_data(:,8)) %tells up number of us solutions
         g1 =  sqrt((ofourx - otwox)^2 + (ofourx - otwox)^2);%17
         v1 = sqrt((a1x - b1x)^2 + (a1y - b1y)^2);%18
         length_total = w+v1+u;%19
+        theta = wz_data(wz,9);%20
+        phi = wz_data(wz,10);%21
+        beta2 = wz_data(wz,11);%22
+        sigma = us_data(us,9);%23
+        psi = us_data(us,10);%24
+        gamma2 = us_data(us,11);%25
         
-        temp = [w,z,u,s,otwox,otwoy,ofourx,ofoury,a1x,a1y,b1x,b1y,a2x,a2y,b2x,b2y,g1,v1,length_total];
-        
-        if solutions_checked > solutions_to_check
-           break; 
-        end
+        temp = [w,z,u,s,otwox,otwoy,ofourx,ofoury,a1x,a1y,b1x,b1y,a2x,a2y,b2x,b2y,g1,v1,length_total,theta,phi,beta2,sigma,psi,gamma2];
         
         if b1x - a1x > 0 && g1 > 0 && v1 > 0
-            if data_pointer <= num_solutions %only save so many solutions
+            if data_pointer <= num_solutions && rand <= prob %only save so many solutions
                 solutions(data_pointer, :) = temp;
                 data_pointer = data_pointer +1;
             end
@@ -187,10 +202,6 @@ for us = 1:length(us_data(:,8)) %tells up number of us solutions
         end
         
     end
-    if solutions_checked > solutions_to_check
-           break; 
-    end
-    
 end
 
 %solution matrix size reduction
